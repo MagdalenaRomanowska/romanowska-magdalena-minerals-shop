014be884bd47d919830d875cb764663493345a82
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2020-12-09 14:28:35 +0100

    Product site styled

--- src/components/views/Post/Post.js
+++ src/components/views/Post/Post.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import styles from "./Post.module.scss";
 
 const Post = ({ post }) => (
@@ -20,19 +21,91 @@ const Post = ({ post }) => (
       <div className={styles.postContent}>
         <div className={styles.detailTitle}>{post ? post.title : ""}</div>
         <div className={styles.detailText}>{post ? post.text : ""}</div>
-        <div className={styles.photoDescription}>{post ? post.photoDescription : ""}</div>
+        <div className={styles.photoDescription}>
+          {post ? post.photoDescription : ""}
+        </div>
         <div className={styles.detailPhoto}>
           <img src={post ? post.photo : ""} alt={"mineralPhoto"} />
           <img src={post ? post.photo2 : ""} alt={"mineralPhoto2"} />
           <img src={post ? post.photo3 : ""} alt={"mineralPhoto3"} />
         </div>
-        <div className={styles.photoDescription2}>{post ? post.photoDescription2 : ""}</div>
-        <div className={styles.detailPhoto}>
-          <img src={post ? post.photoBracelet : ""} alt={"mineralPhoto"} />
-          <img src={post ? post.photoEarings : ""} alt={"mineralPhoto2"} />
-          <img src={post ? post.photoRing : ""} alt={"mineralPhoto3"} />
+        <div className={styles.photoDescription2}>
+          {post ? post.photoDescription2 : ""}
         </div>
-        <div className={styles.postDetailPrice}>Price: {post ? post.price : ""}</div>
+        <div className={styles.detailPhoto}>
+          <div>
+            <img src={post ? post.photoBracelet : ""} alt={"photoBracelet"} />
+            <h2>Bracelet</h2>
+            <div className={styles.postDetailPrice}>
+              Price: {post ? post.price : ""}
+            </div>
+            <div className={styles.selectQuantity}>
+              <div>Select quantity</div>
+              <div className={styles.plusAndMinus}>
+                <div className={styles.minus}>-</div>
+                <div className={styles.amount}>{post.quantity}</div>
+                <div className={styles.plus}>+</div>
+              </div>
+              <div className={styles.totalPrice}>
+                <div className={styles.total}>Total :</div>
+                <div className={styles.totalBox}></div>
+              </div>
+              <div>
+                <Link to={`/myOrder`} className={styles.addToChart}>
+                  Add to chart
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={post ? post.photoEarings : ""} alt={"photoEarings"} />
+            <h2>Earings</h2>
+            <div className={styles.postDetailPrice}>
+              Price: {post ? post.price2 : ""}
+            </div>
+            <div className={styles.selectQuantity}>
+              <div>Select quantity</div>
+              <div className={styles.plusAndMinus}>
+                <div className={styles.minus}>-</div>
+                <div className={styles.amount}>{post.quantity}</div>
+                <div className={styles.plus}>+</div>
+              </div>
+              <div className={styles.totalPrice}>
+                <div className={styles.total}>Total :</div>
+                <div className={styles.totalBox}></div>
+              </div>
+              <div>
+                <Link to={`/myOrder`} className={styles.addToChart}>
+                  Add to chart
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={post ? post.photoRing : ""} alt={"photoRing"} />
+            <h2>Ring</h2>
+            <div className={styles.postDetailPrice}>
+              Price: {post ? post.price3 : ""}
+            </div>
+            <div className={styles.selectQuantity}>
+              <div>Select quantity</div>
+              <div className={styles.plusAndMinus}>
+                <div className={styles.minus}>-</div>
+                <div className={styles.amount}>{post.quantity}</div>
+                <div className={styles.plus}>+</div>
+              </div>
+              <div className={styles.totalPrice}>
+                <div className={styles.total}>Total :</div>
+                <div className={styles.totalBox}></div>
+              </div>
+              <div>
+                <Link to={`/myOrder`} className={styles.addToChart}>
+                  Add to chart
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
