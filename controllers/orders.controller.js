d18528904951f1dd5a07e6732a555e7b8206e156
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2021-10-19 13:19:42 +0200

    Post order controller changed

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -22,17 +22,24 @@ exports.getById = async (req, res) => {
 };
 
 exports.post = async (req, res) => {
+  console.log("entering POST order");
     try {
         const { orders, name, address, phoneNumber, email } = req.body;  
         const orderItems = [];
+        if(orders === undefined) {
+          throw new Error('No order items in order. BTW in free time change orders to orderItems - here and in database');
+        }
         orders.forEach(element => {
           orderItem = new OrderItem({productId: element.productId, orderedItems: element.orderedItems, amountAll: element.amountAll, comment: element.comment });
           orderItems.push(orderItem);
         }); 
         const newOrder = new Order({ orders: orderItems, name, address, phoneNumber, email });
+        console.log("POST order before SAVE");
         await newOrder.save();
+        console.log("POST order after SAVE");
         res.json({ message: 'OK' });
     } catch (err) {
-      res.status(500).json({ message: err });
+      console.log("POST order error", err);
+      res.status(500).json({ message: err.message });
     }
 };
